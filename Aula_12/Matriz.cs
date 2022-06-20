6e7b821649af0defe432833a25627516431ceace
Marcelo alves <marcelloalves09@gmail.com>
2022-06-20 17:09:12 -0300

    Aula 12

---
+++ Aula_12/Matriz.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_12
+{
+    internal class Matriz
+    {
+
+        char[,] Coordenada = new char[3, 3]
+        {
+            { '7', '8', '9' },
+            { '6', '5', '4' },
+            { '3', '2', '1' },
+        };
+    }
+}
+
+
+
---
+++ Aula_12/Todo.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_12
+{
+    internal class Todo
+    {
+       List<String> Tarefas = new List<String>();
+        public void AdicionarTarefa() {
+            Console.WriteLine("Adicione uma tarefa: ");
+            Tarefas.Add(Console.ReadLine());
+        }
+        public void RemoverTarefa() {
+            Console.WriteLine("Qual tarefa deseja remover: ");
+            Tarefas.Remove(Console.ReadLine());
+        }
+
+        public void AtualizarTarefa() {
+            Console.WriteLine("Qual tarefa deseja atualizar? ");
+            string taref = Console.ReadLine();
+            if (Tarefas.Contains(taref))
+            {
+                Console.WriteLine("Qual a nova tarefa?");
+                int index = Tarefas.IndexOf(taref);
+                Tarefas[index] = Console.ReadLine();
+            }
+        }
+        public void ListarTodasAsTarefas() {
+            foreach (var item in Tarefas)
+                Console.WriteLine(item);
+        }
+     
+
+        }
+
+
+
+    }
+    
+
